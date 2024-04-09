2e2c450e4e2ab42a5d966945a74e88a290dedb42
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-04-09 15:13:34 +0900

    build削除

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0d3c365ba08346895790');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c598c4eee5081fd61bbb');
